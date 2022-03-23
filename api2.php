42d7439909cd72ccd52965c6e122b6a65c12a48d
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-03-23 11:03:54 +0200

    Added some comments for API2

--- api2.php
+++ api2.php
@@ -1,26 +1,29 @@
 <?php
-include("DB_info.php");
+include("DB_info.php"); //connecting with the database 
 
-$Amount = floatval($_POST["Amount"]);
+$Amount = floatval($_POST["Amount"]); //getting those data from the application
 $Internal = $_POST["Internal"];
 $External = $_POST["External"];
 $Daily_Rate = floatval($_POST["Daily_Rate"]);
-$Result =0;
-date_default_timezone_set('Asia/Beirut');
+
+$Result =0; //default result
+
+date_default_timezone_set('Asia/Beirut'); //getting date and time for each conversion
 $date = date('Y-m-d H:i:s');
 
-if($Internal == "USD"){
+if($Internal == "USD"){ //converting the ammount to USD or LBP
     $Result = $Amount * $Daily_Rate;
 }
 else{
     $Result = $Amount / $Daily_Rate;
 
 }
+
 $query =   $mysqli->prepare("INSERT INTO conversions_history (amount,internal,external,daily_rate,result,time) VALUES('$Amount','$Internal',
-'$External','$Daily_Rate','$Result','$date')"); 
+'$External','$Daily_Rate','$Result','$date')"); //Storing the conversion to the database ("Conversions_history")
 
-$query->execute();
+$query->execute(); //executing the query
 
 
-echo json_encode($Result);
+echo json_encode($Result); //returning the result to the application as JSON
 ?>
