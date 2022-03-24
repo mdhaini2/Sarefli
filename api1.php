5082a39bc1dc9f77b87201476c2e9338e0835801
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-03-25 01:16:57 +0200

    API 1 and 2 Done

--- api1.php
+++ api1.php
@@ -1,7 +1,9 @@
 <?php 
-    include("simple_html_dom.php");
-    $html= file_get_html("https://lirarate.org");
-    $title = $html->find("//[@id='site-content']");
-    $name = $title[0]->innertext;
-    echo $name;
+    include("simple_html_dom.php"); //library to get built in functions that we used to get content from website 
+
+    $html= file_get_contents("https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP"); //getting the content from api rate that lirarate.org use
+
+    $html_json = json_decode($html,true); // convert $html from type String to json
+    
+    echo json_encode($html_json);   //return the rates to the front end as json
     ?>
--- api2.php
+++ api2.php
@@ -1,14 +1,14 @@
 <?php
 include("DB_info.php"); //connecting with the database 
 
-$Amount = floatval($_POST["Amount"]); //getting those data from the application
+$Amount = floatval($_POST["Amount"]); //get those data from the front end
 $Internal = $_POST["Internal"];
 $External = $_POST["External"];
 $Daily_Rate = floatval($_POST["Daily_Rate"]);
 
 $Result =0; //default result
 
-date_default_timezone_set('Asia/Beirut'); //getting date and time for each conversion
+date_default_timezone_set('Asia/Beirut'); //get date and time for each conversion
 $date = date('Y-m-d H:i:s');
 
 if($Internal == "USD"){ //converting the ammount to USD or LBP
@@ -25,5 +25,5 @@ $query =   $mysqli->prepare("INSERT INTO conversions_history (amount,internal,ex
 $query->execute(); //executing the query
 
 
-echo json_encode($Result); //returning the result to the application as JSON
+echo json_encode($Result); //returning the result to the front end as JSON
 ?>
