e11c714868b3587239475992782184135c6e38b2
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-03-25 13:04:26 +0200

    Handling request error

--- api1.php
+++ api1.php
@@ -1,9 +1,20 @@
 <?php 
     include("simple_html_dom.php"); //library to get built in functions that we used to get content from website 
-
-    $html= file_get_contents("https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP"); //getting the content from api rate that lirarate.org use
-
-    $html_json = json_decode($html,true); // convert $html from type String to json
     
-    echo json_encode($html_json);   //return the rates to the front end as json
+        
+            if($html= file_get_contents("https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP")){//getting the content from api rate that lirarate.org use 
+                
+                $html_json = json_decode($html,true); // convert $html from type String to json
+    
+                echo json_encode($html_json);   //return the rates to the front end as json
+            } 
+            else{ //if we get an error for requesting we will return to the front-end an error message
+                echo json_encode("Request Error");
+            }
+         
+        
+       
+    
+
+   
     ?>
