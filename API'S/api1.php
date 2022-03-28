14f75261e84f91c7964e6c5963395c1dc6bf4fd4
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-03-29 00:49:22 +0300

    Fixed api's dissaster

---
+++ API'S/api1.php
@@ -0,0 +1,13 @@
+<?php 
+    include("simple_html_dom.php"); //library to get built in functions that we used to get content from website 
+    
+        
+            if($html= file_get_contents("https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20223290")){//getting the content from api rate that lirarate.org use 
+    
+                echo json_encode($html);   //return the rates to the front end as json
+            } 
+            else{ //if we get an error for requesting we will return to the front-end an error message
+                echo json_encode("Request Error");
+            }
+
+    ?>
