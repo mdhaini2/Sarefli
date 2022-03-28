94e60f4d71bdb0d8b88fb5c74162fbd0b2d3246e
Jamil Awada <jamilawada2002@gmail.com>
2022-03-28 18:55:55 +0300

    fixed bug that caused crash

--- Sarefli Android Studio Code/app/src/main/java/com/dhaini/sarefli/MainActivity.java
+++ Sarefli Android Studio Code/app/src/main/java/com/dhaini/sarefli/MainActivity.java
@@ -3,12 +3,18 @@ package com.dhaini.sarefli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // These lines remove the action and title bars
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
