c6ee26e517d20db4a86cb32b40f332d9a7b622da
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-03-30 16:12:41 +0300

    fixed display for buy and sell rate

--- Sarefli Android Studio Code/app/src/main/java/com/dhaini/sarefli/MainActivity.java
+++ Sarefli Android Studio Code/app/src/main/java/com/dhaini/sarefli/MainActivity.java
@@ -15,17 +15,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView buyText;
+    TextView sellText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +39,15 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        buyText = (TextView) findViewById(R.id.tv_buyText2);
+        sellText = (TextView) findViewById(R.id.tv_sellText2);
         // API 1 URL
         String url = "http://10.0.2.2/TheBroallers/TheBroallers/api1.php";
-        DownloadTask Task = new DownloadTask();
-        Task.execute(url);
+        apiCaller1 api1 = new apiCaller1();
+        api1.execute(url);
     }
 
-    public class DownloadTask extends AsyncTask<String,Void,String> {
+    public class apiCaller1 extends AsyncTask<String,Void,String> {
         protected String doInBackground(String... urls){
             // URL and HTTP initialization to connect to API 1
             URL url;
@@ -75,11 +82,46 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
         }
+        protected void onPostExecute(String values){
+            super.onPostExecute(values);
+            try{
+                Log.i("Values",values);
+                DecimalFormat formatter = new DecimalFormat("#,###");
+
+                String[] splitValues = values.split("]");
+
+                String[] splitBuy = splitValues[0].split(",");
+                String buyRate = String.valueOf(formatter.format(Integer.parseInt(splitBuy[1]))) ;
+                String buyTimeStamp = splitBuy[0].substring(1);
+
+                Log.i("TimeStamp",convertTimeStamp(buyTimeStamp).toString());
+                Log.i("Buy rate",buyRate);
+
+                String[] splitSell = splitValues[1].split(",");
+                String sellRate = String.valueOf(formatter.format(Integer.parseInt(splitSell[1]))) ;
+                String sellTimeStamp = splitBuy[0].substring(1);
+
+                buyText.setText("1 USD at " + buyRate + " LBP");
+                sellText.setText("1 USD at " + sellRate + " LBP");
+
+            }
+            catch(Exception e){
+                e.printStackTrace();
+
+
+            }
+        }
     }
 
     public void goToCalculator(View view){
         // Create an intent and start the the calculator activity
         Intent i1 = new Intent(getApplicationContext(), Calculator.class);
         startActivity(i1);
+    }
+
+    public Date convertTimeStamp(String timeStamp){
+        long time = Long.parseLong(timeStamp);
+        Date date = new Date(time);
+        return date;
     }
 }
